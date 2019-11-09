6980d1480bb7e3fb5a0005f98ab499d56f6023be
Scglynn <53241290+Scglynn@users.noreply.github.com>
2019-11-09 09:47:36 -0600

    Add files via upload

---
+++ StandardInputScanner/StandardInputScanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+//C:\Users\scgly\AppData\Local\Programs\Microsoft VS Code
+
+public class StandardInputScanner {
+    public static void main(String[] args) throws Exception
+    {
+
+    Scanner sc = new Scanner(System.in);
+    {
+    System.out.println("First name: " + sc.nextLine());
+    System.out.println("Last name:  " + sc.nextLine());
+    System.out.println("DOB:        " + sc.nextLine());
+    System.out.println("Major:      " + sc.nextLine());
+    }
+}
+}
