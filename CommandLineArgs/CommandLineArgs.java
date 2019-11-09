ec47a6b50efd64dcfe944f2865b3610b2a411a73
Scglynn <53241290+Scglynn@users.noreply.github.com>
2019-11-09 09:43:16 -0600

    Add files via upload

---
+++ CommandLineArgs/CommandLineArgs.java
@@ -0,0 +1,8 @@
+public class CommandLineArgs {
+    public static void main (String [] args){
+        System.out.println("First name: " + args[0]);
+        System.out.println("Last name:  " + args[1]);
+        System.out.println("DOB:        " + args[2]);
+        System.out.println("Major:      " +args[3]);
+    }
+}
