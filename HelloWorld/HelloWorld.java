6eb65bd4397345a706cb0b11425847011715ace3
Scglynn <53241290+Scglynn@users.noreply.github.com>
2019-11-09 09:45:35 -0600

    Add files via upload

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+// This program prints the message "Hello, World!" on the screen.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
